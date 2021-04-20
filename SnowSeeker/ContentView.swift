c4b03f586af777becc9fd58bba578b57dd684f79
Paul Houghton <paul.houghton@gmail.com>
2021-04-20 08:25:01 +0100

    First Commit.

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,10 +7,110 @@
 
 import SwiftUI
 
+//struct User: Identifiable {
+//    var id = "Taylor Swift"
+//}
+//
+//struct UserView: View {
+//    var body: some View {
+//        Group {
+//            Text("Name Paul")
+//            Text("Country: England")
+//            Text("Pets: Luna, Arya, and Toby")
+//        }
+//    }
+//}
+
 struct ContentView: View {
+//    @State private var selectedUser: User? = nil
+    
+//    @State private var layoutVertically = false
+
+//    @Environment(\.horizontalSizeClass) var sizeClass
+    
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+     
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(destination: Text(resort.name)) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5.0)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(headline)
+
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+
+            }
+            .navigationBarTitle("Resorts")
+        }
+        
+//        Text("Hello, world!")
+//            .padding()
+        
+//        Group {
+//            if sizeClass == .compact {
+//                VStack(content: UserView.init)
+//            }
+//            else {
+//                HStack(content: UserView.init)
+//            }
+//        }
+        
+//        Group {
+//            if layoutVertically {
+//                VStack {
+//                    UserView()
+//                }
+//            }
+//            else {
+//                HStack {
+//                    UserView()
+//                }
+//            }
+//        }
+//        .onTapGesture {
+//            self.layoutVertically.toggle()
+//        }
+        
+//        Text("Hello, world!")
+//            .padding()
+        
+//        Text("Hello, world!")
+//            .padding()
+//            .onTapGesture {
+//                self.selectedUser = User()
+//            }
+//            .alert(item: $selectedUser) { user in
+//                Alert(title: Text(user.id))
+//            }
+        
+//        NavigationView {
+//            NavigationLink(destination: Text("New secondary")) {
+//                Text("Hello, world!")
+//                    .padding()
+//                    .navigationBarTitle("Primary")
+//            }
+//
+//            Text("Secondary")
+//        }
     }
 }
 
